b64e429f566603ae5bf6a9ceaad070092f86084b
Jacob Qvortrup <jfq@arosii.dk>
2024-12-16 07:55:00 +0100

    BiWeekly backend done

--- src/main/java/dk/jfq/bcal/BusyCalendar.java
+++ src/main/java/dk/jfq/bcal/BusyCalendar.java
@@ -1,9 +1,7 @@
 package dk.jfq.bcal;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.file.Paths;
+import java.io.InputStream;
 import java.time.DayOfWeek;
 import java.time.Duration;
 import java.time.LocalDate;
@@ -12,29 +10,36 @@ import java.util.stream.IntStream;
 
 public class BusyCalendar {
 
-    private static final int NUMBER_OF_WEEKS = 4;
+    private final List<BusyWeek> busyWeeks;
 
-    public static void main(String[] args) throws IOException {
-        final LocalDate firstMonday = getMondayInThisWeek();
-        List<BusyWeek> busyWeeks = IntStream.range(0, NUMBER_OF_WEEKS).boxed().map(i -> new BusyWeek(firstMonday.plusDays(i * 7L), Duration.ofMinutes(15))).toList();
+    public BusyCalendar(InputStream iCalendarStream, LocalDate from, int numberOfWeeks, Duration slotDuration) throws IOException {
+        LocalDate monday = getMondayInWeek(from);
 
-        try (BufferedInputStream in = new BufferedInputStream(new FileInputStream(Paths.get("/Users/jfq/Downloads/jfq.ical").toFile()))) {
-            try (BusyEventsReader busyEventsReader = new BusyEventsReader(in, firstMonday, firstMonday.plusDays(NUMBER_OF_WEEKS * 7))) {
-                for (BusyEvent busyEvent : busyEventsReader) {
-                    for (BusyWeek busyWeek : busyWeeks) {
-                        busyWeek.add(busyEvent);
-                    }
+        this.busyWeeks = IntStream.range(0, numberOfWeeks).boxed().map(i -> new BusyWeek(monday.plusDays(i * 7L), slotDuration)).toList();
+
+        try (BusyEventsReader busyEventsReader = new BusyEventsReader(iCalendarStream, monday, monday.plusDays(numberOfWeeks * 7L))) {
+            for (BusyEvent busyEvent : busyEventsReader) {
+                for (BusyWeek busyWeek : busyWeeks) {
+                    busyWeek.add(busyEvent);
                 }
             }
         }
-        busyWeeks.forEach(System.out::println);
     }
 
-    private static LocalDate getMondayInThisWeek() {
-        LocalDate monday = LocalDate.now();
+    private static LocalDate getMondayInWeek(LocalDate weekDay) {
+        LocalDate monday = weekDay;
         while (monday.getDayOfWeek() != DayOfWeek.MONDAY) {
             monday = monday.minusDays(1);
         }
         return monday;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (BusyWeek busyWeek : busyWeeks) {
+            sb.append(busyWeek).append('\n').append('\n');
+        }
+        return sb.toString();
     }
 }
